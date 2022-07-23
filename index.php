94a9ad8bc141c0c7d600a5deb6f533631a16c3ed
Abject <lucaswurtz0@gmail.com>
2022-07-23 21:55:48 +0200

    soirée du 23.07

--- index.php
+++ index.php
@@ -1,42 +1,39 @@
 <?php
 
-echo "<h1>Hello world</h1>";
+$value = $_POST['value'] ?? '';
 
-$a = 5;
-$b = 12;
-
-function calculator1($a, $b)
-{
-    $c = $a + $b;
-    echo "<h3>$a + $b = $c</h3>";
+if ($value < 10) {
+    $phrase1 = '<h2>Je suis inférieur a 10</h2>';
+} elseif ($test > 10) {
+    $phrase2 = '<h2>Je suis supérieur a 10</h2>';
 }
 
-calculator1($a, $b);
+$bdd = new PDO('mysql:host=localhost;dbname=test_php;charset=utf8', 'root', '');
 
-$data = [
-    $user1 = [
-        "firstName" => "Lucas",
-        "lastName" => "Wurtz",
-        "pseudo" => "Abject",
-        "age" => 22
-    ],
-    $user2 = [
-        "firstName" => "Marjorie",
-        "lastName" => "Salingue",
-        "Pseudo" => "Tael",
-        "age" => 20
-    ]
-];
+?>
 
-function test($data, $user1, $user2)
-{
-    $firstUser = $data[$user1];
+<html lang="en">
 
-    echo "<ref";
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TEST PHP & SQL</title>
+    <link rel="stylesheet" href="./main.css">
+</head>
 
-    var_dump("<h2>$firstUser</h2>");
+<body>
+    <?php echo '<h1>Bonjour le monde</h1>' ?>
+    <?php print_r('' ?? $phrase1 ?? $phrase2)  ?>
+    <form action="http://localhost:80/test_php/index.php" method="POST">
+        <label for="name" id="name">Name</label>
+        <input type="text" name="name" id="name_input">
+        <label for="age" id="age">Age</label>
+        <input type="text" name="age" id="age_input">
+        <label for="value" id="value">Votre valeur</label>
+        <input type="text" name="value" id="value_input">
+        <input type="submit">
+    </form>
+</body>
 
-    echo "<ref";
-}
-
-test($data, $user1, $user2);
+</html>
